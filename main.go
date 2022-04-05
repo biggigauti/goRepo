74add0f35180efdbae4ac58b1e83fa8d4bf697eb
biggigauti <98067108+biggigauti@users.noreply.github.com>
2022-04-05 09:34:50 -0600

    Update main.go

--- main.go
+++ main.go
@@ -5,29 +5,29 @@ import (
 	"net/http"
 )
 
-type ginItem struct {
-	ID    string  `json:"id"`
+type ginItem struct { //Collection of fields.
+	ID    string  `json:"id"` //json fields
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
-var ginList = []ginItem{
+var ginList = []ginItem{ //List of type ginItem
 	{ID: "1", Name: "Ólafsson Gin", Price: 39.99},
 	{ID: "2", Name: "New Amsterdam Gin", Price: 39.99},
 	{ID: "3", Name: "Kuro", Price: 39.99},
 }
 
-func getGins(c *gin.Context) {
+func getGins(c *gin.Context) { //Get function to grab gin list
 	c.IndentedJSON(http.StatusOK, ginList)
 }
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*.tmpl")
-	r.Static("/images", "./images")
+	r.LoadHTMLGlob("templates/*.tmpl") //load html templates
+	r.Static("/images", "./images") //load static files
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{ //load index.tmpl at / extension
 			"title": "Gin & Gonic Mixing",
 		})
 	})
